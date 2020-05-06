3e971db7ed40615cdb63573a109eaca45b13072a
raghuveercoding <64122717+raghuveercoding@users.noreply.github.com>
2020-05-06 13:56:22 -0400

    discount countries

--- week_03/day_1/assignments/day_01/countries.js
+++ week_03/day_1/assignments/day_01/countries.js
@@ -2,14 +2,59 @@ function program(input) {
     // input
     // console.log(input)
     // write code here
-  switch (input) {
-  case India:
-    input = "Asia";
-    break;
-  }
-  return("input")
-  
      
+    switch (input) {
+      case "India":
+        return ('Asia')
+        break;
+        case "United States of America":
+        return('North America')
+        break;
+        case "China":
+        return ('Asia')
+        break;
+        case "Nepal":
+          return ('Asia')
+        break;
+        case "Australia":
+          return ('Australia')
+        break;
+        case "France":
+        return ('Europe')
+        break;
+        case "Germany":
+        return ('Europe')
+        break;
+        case "South Africa":
+          return ('Africa')
+        break;
+        case "Nigeria":
+          return ('Africa')
+            break;
+            case "Mexico":
+              return ('North America')
+        break;
+        case "Argentina":
+        return ('South America')
+        break;
+        case "Brazil":
+          return ('South America')
+        break;
+        case "Canada":
+          return ('North America')
+        break;
+        case "Singapore":
+          return ('Asia')
+        break;
+        case "Sweden":
+          return ('Europe')
+        break;
+        return("input");
+    }
+     
+    return("input");
+        
+    
 
     // return output
 }
--- week_03/day_1/assignments/day_01/discount.js
+++ week_03/day_1/assignments/day_01/discount.js
@@ -2,6 +2,21 @@ function program(input) {
     // input
     // console.log(input)
     // write code here
+    var total
+    var dis
+    if (input<300){
+        return input + " " + 0 + " " + input
+    }
+    else if (input>=300 && input<=1000){
+        dis = input * 0.1
+        total = input - dis
+       return input + " " + dis + " " + total
+    }
+    else (input>1000);{
+        dis = 100
+        total = input - dis
+        return input + " " + dis + " " + total
+        }
 
 
     // return output
