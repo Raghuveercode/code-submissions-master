c6799252c091aedff9ed7a72b0c44ee743d62777
raghuveercoding <64122717+raghuveercoding@users.noreply.github.com>
2020-05-06 09:30:59 -0400

    agecheck

---
+++ week_03/day_1/assignments/day_01/ageCheck.js
@@ -0,0 +1,20 @@
+function program(input) {
+    // input
+    // console.log(input)
+    // write code here
+
+    if(input>18){
+        return("You can drive in India!");
+    } 
+     else if(input<18){
+       return("You can't drive in India!");
+        }
+            else{
+       return("You are just old enough to drive in India!");
+        } 
+        
+    // return output
+}
+
+
+module.exports = program
---
+++ week_03/day_1/assignments/day_01/countries.js
@@ -0,0 +1,18 @@
+function program(input) {
+    // input
+    // console.log(input)
+    // write code here
+  switch (input) {
+  case India:
+    input = "Asia";
+    break;
+  }
+  return("input")
+  
+     
+
+    // return output
+}
+
+
+module.exports = program
